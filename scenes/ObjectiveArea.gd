d13985b4d383f61dcf57fe62267fd43f048c5781
nabilmuafa <nmuafa@gmail.com>
2025-02-21 19:45:26 +0700

    finish new level

--- scenes/ObjectiveArea.gd
+++ scenes/ObjectiveArea.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@onready var win_screen = get_tree().current_scene.find_child("WinScreen", true, false)
+
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
 	if (body.name == "BlueShip"):
 		print("Reached objective!")
+		win_screen.visible = true
+		await get_tree().create_timer(2.0).timeout  # Wait for 2 seconds
+		get_tree().change_scene_to_file("res://scenes/Level2.tscn")
